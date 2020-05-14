1ec7f09790c45e14896d3e237f9dd23d0cbf23ab
fab <fin.albis@gmail.com>
2020-05-14 12:22:45 +0800

    Create install_zsh.sh

---
+++ zsh_config/install_zsh.sh
@@ -0,0 +1,15 @@
+# /usr/bin/bash
+apt update
+sudo apt install -y zsh git fontconfig unzip autojump
+echo $SHELL
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+wget https://raw.githubusercontent.com/fablol/public_config/master/zsh_config/.zshrc
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /usr/share/fonts/Meslo
+fc-cache -f -v
+chsh -s $(which zsh)
+# source ~/.zshrc
