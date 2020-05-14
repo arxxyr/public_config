51cf349403da41141266a455fc7908d1bf591126
fab <fin.albis@gmail.com>
2020-05-14 13:23:12 +0800

    Update install_zsh.sh

--- zsh_config/install_zsh.sh
+++ zsh_config/install_zsh.sh
@@ -8,8 +8,8 @@ git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${Z
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 wget https://raw.githubusercontent.com/fablol/public_config/master/zsh_config/.zshrc
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d /usr/share/fonts/Meslo
-fc-cache -f -v
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+# unzip Meslo.zip -d /usr/share/fonts/Meslo
+# fc-cache -f -v
 chsh -s $(which zsh)
 # source ~/.zshrc
